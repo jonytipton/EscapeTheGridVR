9d17ec6fc256207cdeea325ed1f11b5b6afe957d
Jonathan Tipton <jonathan.tipton1998@gmail.com>
2021-04-16 00:49:01 -0600

    Throwing
    Disk thrown and returns to hand. Able to grab properly

--- Assets/DiscThrow.cs
+++ Assets/DiscThrow.cs
@@ -9,6 +9,12 @@ public class DiscThrow : MonoBehaviour
     private Material trailMaterial;
     private GameObject disc;
     private XRGrabInteractable grabInteractable = null;
+    private bool discGrabbed = false;
+    private bool discReturned = false;
+    public float discReturnDelay = 3f;
+    private float discReturnCountdown;
+
+    public GameObject throwingHand;
 
     // Start is called before the first frame update
     void Start()
@@ -16,31 +22,52 @@ public class DiscThrow : MonoBehaviour
         disc = gameObject;
         trailMaterial = disc.GetComponent<TrailRenderer>().material;
         grabInteractable = GetComponent<XRGrabInteractable>();
-   
+        discReturnCountdown = discReturnDelay;
+
         //Listeners
-        grabInteractable.onSelectEntered.AddListener(Grabbed);
-        grabInteractable.onSelectExited.AddListener(Released);
+        //grabInteractable.onSelectEntered.AddListener(Grabbed);
+        //grabInteractable.onSelectExited.AddListener(Released);
+
     }
 
-    private void Released(XRBaseInteractor arg0) {
-        print("released. Enabling Trail");
+    public void Released(XRBaseInteractor arg0) {
+        Debug.Log("Disc Released. Enabling Trail");
         disc.GetComponent<TrailRenderer>().enabled = true;
+        discGrabbed = false;
+        discReturned = false;
+        //add rotation
     }
 
-    private void Grabbed(XRBaseInteractor arg0) {
-        Debug.Log("Grabbed Disc");
+    public void Grabbed(XRBaseInteractor arg0) {
+        Debug.Log("Grabbed Disc. Disabling Trail!");
+        discGrabbed = true;
+        discReturned = true;
+        disc.GetComponent<TrailRenderer>().enabled = false;
+        discReturnCountdown = discReturnDelay;
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        if (!discGrabbed && !discReturned) {
+                discReturnCountdown -= Time.deltaTime;
+            if (discReturnCountdown <= 0) {
+                ReturnDisc();
+            }
+        }
+    }
 
+    private void ReturnDisc() {
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+        transform.position = Vector3.MoveTowards(transform.position, throwingHand.transform.position, .35f);
     }
     
     private void OnCollisionEnter(Collision collision) {
-        print("hit");
+        //Debug.Log("Hit Surface. Changing material base color!");
         Color newColor = UnityEngine.Random.ColorHSV(0f, 1f, .5f, 1f, .5f, 1f);
         trailMaterial.SetColor("_BaseColor", newColor);
     }
+
+
 }
