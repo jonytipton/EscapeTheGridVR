a748c503d1a57817e2aab234d05a89c06ca43eb0
Jonathan Tipton <jonathan.tipton1998@gmail.com>
2021-05-02 23:47:48 -0600

    Enemy Throw
    Modified animation (added animation event to timeline) that allows enemy to throw disc in direction of player.

---
+++ Assets/EnemyThrow.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyThrow : MonoBehaviour
+{
+    public GameObject enemyDisc;
+    public  Transform discSpawnPoint;
+    public Animator enemyAnimator;
+    public float throwCountdown = 5f;
+    public GameObject player;
+    public bool thrown = false;
+    public float speed = 5;
+    public float discSpawnCountdown = 10;
+    bool discReady = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        throwCountdown -= Time.deltaTime;
+        if (throwCountdown <= 0 && discReady) {
+            //trigger throw animation, animation event in timeline will trigger ThrowDisc() below
+            enemyAnimator.SetBool("Throw", true);
+            discReady = false;
+        }
+        if (thrown) {
+            discSpawnCountdown -= Time.deltaTime;
+
+            if (discSpawnCountdown <= 0) {
+                SpawnEnemyDisc();
+            }
+        }
+    }
+
+    void ThrowDisc() {
+        Debug.Log("Throw Enemy Disc");
+        enemyDisc.transform.SetParent(null);
+        Vector3 direction = player.transform.position - enemyDisc.transform.position;
+        direction.Normalize();
+        enemyDisc.GetComponent<Rigidbody>().AddForce(direction * 1000 * speed);
+        thrown = true;
+    }
+
+    void SpawnEnemyDisc() {
+        enemyDisc.GetComponent<Rigidbody>().useGravity = false;
+        enemyDisc.transform.position = discSpawnPoint.position;
+        enemyDisc.transform.SetParent(discSpawnPoint);
+        discReady = true;
+        discSpawnCountdown = 10;
+        throwCountdown = 10;
+        thrown = false;
+    }
+}
---
+++ Assets/Scripts/UpDown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDown : MonoBehaviour
+{
+    public float maxSpeed;
+    private float speed;
+    private float newY;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(1f, maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newY = Mathf.Sin(Time.time * speed) * height;
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x, newY, pos.z);
+    }
+}
